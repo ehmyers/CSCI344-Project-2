230f46e0e8968a627a94aaf07db844e73c2d74b4
Emily Myers <emilyhmyers@gmail.com>
2013-02-13 18:45:22 -0500

    changes the tweets displayed based on user input

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -2,28 +2,28 @@ var main = function () {
 
     var twitter = new ctwitter.CTwitter();
 	var numTweets = 0;
-    var tweetTopic = "hello";
+    var tweetTopic = "";
 
-    //$("#user_input_button").click(function() {
+    $("#user_input_button").click(function() {
         // add input to result div
-        //tweetTopic = ("#user_input").val();
-        //console.log(("#user_input").val());
-    //}
+        tweetTopic = ($("#user_input").val());
+        console.log(tweetTopic);
 
-	twitter.stream("statuses/filter", { lang:"en", track:[tweetTopic] }, function (stream) {
-		stream.on("data", function (tweet) {
-  		numTweets += 1;
-			$("<p>" + numTweets + ". " + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
+    	twitter.stream("statuses/filter", { lang:"en", track:[tweetTopic] }, function (stream) {
+    		stream.on("data", function (tweet) {
+      		numTweets += 1;
+    			$("<p>" + numTweets + ". " + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
 
-    	// making sure there are only ten tweets on page
-    	if(numTweets > 10) {
-    		// yo dawg, I heard you like $()
-    		$($("#tweets").children()[10]).fadeOut(function() {
-    			$(this).remove();
-    		});
-    	}
-	  });
-	});
+        	// making sure there are only ten tweets on page
+        	if(numTweets > 10) {
+        		// yo dawg, I heard you like $()
+        		$($("#tweets").children()[10]).fadeOut(function() {
+        			$(this).remove();
+        		});
+        	}
+    	  });
+    	});
+    });
 }
 
 $(document).ready(main);
