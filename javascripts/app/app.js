5a289cdb0dcd54fc782fa6523a75d027b52deca6
Emily Myers <emilyhmyers@gmail.com>
2013-02-16 19:29:40 -0500

    fixes javascript indentation

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,37 +1,41 @@
 var main = function () {
 
-    var twitter = new ctwitter.CTwitter();
+	var twitter = new ctwitter.CTwitter();
 	var numTweets = 0;
-    var tweetTopic = "";
+	var tweetTopic = "";
+	var tweetUsername = "";
 
-    $("#user_input_button").click(function() {
-        $("#tweets").hide();
-        // add input to result div
-        tweetTopic = ($("#user_input").val());
-        console.log(tweetTopic);
+	$("#user_input_button").click(function() {
+		$("#tweets").hide();
+		// add input to result div
+		tweetTopic = ($("#user_input").val());
+		console.log(tweetTopic);
 
-    	twitter.stream("statuses/filter", { lang:"en", track:[tweetTopic] }, function (stream) {
-    		stream.on("data", function (tweet) {
-      		numTweets += 1;
-    			$("<p>" + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
+		twitter.stream("statuses/filter", { lang:"en", track:[tweetTopic] }, function (stream) {
+			stream.on("data", function (tweet) {
+				numTweets += 1;
+				$("<p>" + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
+				// tweetUsername = $(tweet.username);
+				// console.log(tweetUsername);
 
-        	// making sure there are only ten tweets on page
-        	if(numTweets > 10) {
-        		// yo dawg, I heard you like $()
-        		$($("#tweets").children()[10]).fadeOut(function() {
-        			$(this).remove();
-        		});
-        	}
-    	  });
-    	});
+				// making sure there are only ten tweets on page
+				if(numTweets > 10) {
+					// yo dawg, I heard you like $()
+					$($("#tweets").children()[10]).fadeOut(function() {
+						$(this).remove();
+					});
+				}
 
-        // on submit, switch screens
-        // hide first screen
-        $("#interface_div").fadeOut();
+			});
+		});
 
-        // show second screen
-        $("#tweets").fadeIn();
-    });
+		// on submit, switch screens
+		// hide first screen
+		$("#interface_div").fadeOut();
+
+		// show second screen
+		$("#tweets").fadeIn();
+	});
 }
 
 $(document).ready(main);
