d28b7c1d6a6d5b9ceb3624acbf41f54e19058d7a
Emily Myers <emilyhmyers@gmail.com>
2013-02-16 22:23:04 -0500

    fixes some of the JSLint problems

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,44 +1,44 @@
 var main = function () {
 
-	var twitter = new ctwitter.CTwitter();
-	var numTweets = 0;
-	var tweetTopic = "";
-	var tweetUsername = "";
+  var twitter = new ctwitter.CTwitter();
+  var numTweets = 0;
+  var tweetTopic = "";
+  var tweetUsername = "";
 
-	$("#user_input_button").click(function() {
-		$("#tweets").hide();
-		// add input to result div
-		tweetTopic = ($("#user_input").val());
-		console.log(tweetTopic);
+  $("#user_input_button").click(function () {
+    $("#tweets").hide();
+    // add input to result div
+    tweetTopic = ($("#user_input").val());
+    console.log(tweetTopic);
 
-		twitter.stream("statuses/filter", { lang:"en", track:[tweetTopic] }, function (stream) {
-			stream.on("data", function (tweet) {
-				numTweets += 1;
-				$("<p>" + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
-				// tweetUsername = $(tweet.username);
-				// console.log(tweetUsername);
+    twitter.stream("statuses/filter", { lang: "en", track: [tweetTopic] }, function (stream) {
+      stream.on("data", function (tweet) {
+        numTweets += 1;
+        $("<p>" + tweet.text + "</p>").prependTo("#tweets").hide().fadeIn(400);
+        // tweetUsername = $(tweet.username);
+        // console.log(tweetUsername);
 
-				// making sure there are only ten tweets on page
-				if(numTweets > 10) {
-					// yo dawg, I heard you like $()
-					$($("#tweets").children()[10]).fadeOut(function() {
-						$(this).remove();
-					});
-				}
+        // making sure there are only ten tweets on page
+        if (numTweets > 10) {
+          // yo dawg, I heard you like $()
+          $($("#tweets").children()[10]).fadeOut(function () {
+            $(this).remove();
+          });
+        }
 
-			});
-		});
+      });
+    });
 
-		// on submit, switch screens
-		// hide first screen
-		$("#interface_div").fadeOut();
+    // on submit, switch screens
+    // hide first screen
+    $("#interface_div").fadeOut();
 
-		// show second screen
-		$("#tweets").fadeIn();
-	});
+    // show second screen
+    $("#tweets").fadeIn();
+  });
 
-	// uses jquery plugin, resizes h1 
-	jQuery("h1").fitText();
+  // uses jquery plugin, resizes h1 
+  jQuery("h1").fitText();
 }
 
 $(document).ready(main);
